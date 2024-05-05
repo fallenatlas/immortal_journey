bc67007310f2c6d9da13363698ba260e32c766d0
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2024-05-05 21:01:54 +0100

    Add visuals for courage and HP

--- global_scripts/Game.gd
+++ global_scripts/Game.gd
@@ -1,4 +1,20 @@
 extends Node
 
-var playerHP : int = 10
-var courage : int = 0
+var playerHP : float = 10.0
+var courage : float = 50.0
+
+var maxHP : float = 10.0
+var minHP : float = 0.0
+
+var maxCourage : float = 100.0
+var minCourage : float = 0.0
+
+var maxSat : float = 1.2
+var minSat : float = 0.3
+
+var maxBri : float = 0
+var minBri : float = -0.3
+
+var redColor : Color = Color(30, 0, 0)
+var whiteColor : Color = Color(30, 30, 30)
+var blackColor : Color = Color(0, 0, 0)

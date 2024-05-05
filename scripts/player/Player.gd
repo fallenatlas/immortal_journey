de745f0bc73fd210b0330b55c70c0e6df80561a7
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2024-05-05 15:46:06 +0100

    Create Dash

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+const DASH_SPEED = 800.0
+const DASH_LENGHT = 0.2
+
+@onready var Dash = $Dash
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimationPlayer")
@@ -12,6 +17,12 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	#Handle Dash
+	if Input.is_action_just_pressed("dash") && Dash.is_cooldown(): # && Game.courage >= 50
+		Dash.start_dash()
+	
+	var speed = DASH_SPEED if Dash.is_dashing() else SPEED
+		
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -26,11 +37,11 @@ func _physics_process(delta):
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 		if velocity.y == 0:
 			anim.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 		if velocity.y == 0:
 			anim.play("Idle")
 			

5d6381e869815966a86b40ebc3657aadb1ff1b8f
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2024-05-06 14:12:37 +0100

    Fix Dash AfterImage

--- scripts/player/DashEffect.gd
+++ scripts/player/DashEffect.gd
@@ -1,18 +1,20 @@
 extends GPUParticles2D
 
+var normaltexture
+var textureInverted
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#texture = load("res://Martial Hero/PNG/Idle/1.png")
-	pass
-
+	normaltexture = load("res://Martial Hero/PNG/Idle/1.png")
+	textureInverted = load("res://Martial Hero/PNG/Idle/1Inverted.png")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#texture = sprite.texture
 	#BUG: Can't flip the texture to when you dash to the left
-	var img : Texture2D = self.texture;
 	
-	#if(!get_parent().get_direction() == 1):
-		#img.flip_x()
-		
-	#self.texture = img
+	if(!get_parent().get_direction() == 1):
+		texture = textureInverted
+	else:
+		texture = normaltexture
+	

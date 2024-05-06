36d34008d6826e00c9ce1ecc9bce97c0c5762776
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2024-05-06 01:57:10 +0100

    Add effect to Dash
    It's bugged when dashing to the left

--- scripts/player/Dash.gd
+++ scripts/player/Dash.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-
 @onready var cooldown_timer = $"Dash Cooldown"
 @onready var dash_timer = $"Dash Time"
+@onready var effect_timer = $"Effect Timer"
 
 func start_dash():
 	cooldown_timer.start();
 	dash_timer.start();
+	
+func start_dash_effect():
+	effect_timer.start();
 	
 func is_cooldown():
 	return cooldown_timer.is_stopped();
 	
 func is_dashing():
 	return !dash_timer.is_stopped();
+	
+func is_effect_cooldown():
+	return effect_timer.is_stopped();
 
--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -7,12 +7,18 @@ const DASH_SPEED = 800.0
 const DASH_LENGHT = 0.2
 
 @onready var Dash = $Dash
+@onready var DashEffect = $DashEffect
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var speed
+
 @onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -20,9 +26,20 @@ func _physics_process(delta):
 	#Handle Dash
 	if Input.is_action_just_pressed("dash") && Dash.is_cooldown(): # && Game.courage >= 50
 		Dash.start_dash()
+		DashEffect.emitting = true
 	
-	var speed = DASH_SPEED if Dash.is_dashing() else SPEED
-		
+	#var speed = DASH_SPEED if Dash.is_dashing() else SPEED
+	if Dash.is_dashing():
+		#speed = DASH_SPEED
+		if(direction == 1):
+			position += Vector2(10, 0)
+		else:
+			position += Vector2(-10, 0)
+		velocity.y = 0
+
+	else:
+		speed = SPEED
+		DashEffect.emitting = false
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -30,12 +47,12 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
+	
 	
 	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
+		get_node("Sprite2D").flip_h = true
 	elif direction == 1:
-		get_node("AnimatedSprite2D").flip_h = false
+		get_node("Sprite2D").flip_h = false
 	if direction:
 		velocity.x = direction * speed
 		if velocity.y == 0:
@@ -53,3 +70,7 @@ func _physics_process(delta):
 	if Game.playerHP <= 0:
 		self.queue_free()
 		get_tree().change_scene_to_file("res://scenes/main_menu/main.tscn")
+
+func get_direction():
+	return Input.get_axis("move_left", "move_right")
+

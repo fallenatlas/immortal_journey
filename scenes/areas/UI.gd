2d2df19ca9685a1c0b9f0be3736e4ec116b3f3c4
Joao Vitor <joaobvitor@gmail.com>
2024-05-12 23:24:30 +0100

    fix last commit by adding untracked files

---
+++ scenes/areas/UI.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+@onready var MUSIC_BUS_ID = AudioServer.get_bus_index("Music")
+@onready var menu = %Menu
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		menu.visible = !menu.visible
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(MUSIC_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(MUSIC_BUS_ID, value < 0.05)
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
+	AudioServer.set_bus_mute(SFX_BUS_ID, value < 0.05)
---
+++ scenes/player/Audio_Player.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+var audio_jump = preload("res://Audio/SFX/12_Player_Movement_SFX/30_Jump_03.wav")
+var audio_attack = preload("res://Audio/SFX/12_Player_Movement_SFX/56_Attack_03.wav")
+var audio_switch_world = preload("res://Audio/SFX/12_Player_Movement_SFX/88_Teleport_02.wav")
+var audio_dash = preload("res://Audio/SFX/8_Atk_Magic_SFX/25_Wind_01.wav")
+
+var audio_node = null
+
+func _ready():
+	audio_node = get_node("AudioStreamPlayer")
+	audio_node.finished.connect(destroy_self)
+	audio_node.stop()
+
+func play_sound(sound_name, position=null):
+
+	if audio_jump == null or audio_attack == null or audio_switch_world == null:
+		print ("Audio not set!")
+		queue_free()
+		return
+
+	if sound_name == "Jump":
+		audio_node.stream = audio_jump
+	elif sound_name == "Attack":
+		audio_node.stream = audio_attack
+	elif sound_name == "Switch_world":
+		audio_node.stream = audio_switch_world
+	elif sound_name == "Dash":
+		audio_node.stream = audio_dash
+	else:
+		print ("UNKNOWN STREAM")
+		queue_free()
+		return
+
+	# If you are using an AudioStreamPlayer3D, then uncomment these lines to set the position.
+	#if audio_node is AudioStreamPlayer3D:
+	#    if position != null:
+	#        audio_node.global_transform.origin = position
+
+	audio_node.play()
+
+
+func destroy_self():
+	audio_node.stop()
+	queue_free()

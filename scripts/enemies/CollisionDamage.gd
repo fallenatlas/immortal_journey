603e1191861190c6293ca4c345a8f3eeef8b11b8
xhmueel <samuelbrazpinto@tecnico.ulisboa.pt>
2024-05-13 10:44:38 +0100

    Fix Dash

--- scripts/enemies/CollisionDamage.gd
+++ scripts/enemies/CollisionDamage.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 func _on_body_entered(body):
-	if body.name == "Player":
-		Game.take_damage(3)
+	#if body.name == "Player":
+		#Game.take_damage(3)
+	pass
 
 #func _process(delta):
 	#var bodies = get_overlapping_bodies()

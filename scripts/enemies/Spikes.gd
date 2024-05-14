60405dbd3045ff1d5d3ca62091d0f7b2cda2df34
jdbaracho <74615959+jdbaracho@users.noreply.github.com>
2024-05-14 23:43:43 +0100

    fix: implement spike continuous damage

--- scripts/enemies/Spikes.gd
+++ scripts/enemies/Spikes.gd
@@ -1,5 +1,30 @@
 extends Node2D
 
 
+const DAMAGE_TICK = 1.0
+
+
+var damageTime = 0.0
+var isTouching = false
+
+
+func _process(delta):
+	if not isTouching:
+		return
+		
+	damageTime += delta
+	if damageTime >= DAMAGE_TICK:
+		damageTime = 0.0
+		Game.take_damage(1, true)
+
+
 func _on_player_detection_body_entered(body):
-	Game.take_damage(1, true)
+	if body.name == "Player":
+		isTouching = true
+		Game.take_damage(1, true)
+		damageTime = 0.0
+
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		isTouching = false

f7866ce470400ff7dc0d7dfb45a8d7aa5982b370
fallenatlas <tiago.a.p.antunes@gmail.com>
2024-06-01 20:47:39 +0100

    improve arrow look and disable enemy health bar on death realm

--- scripts/enemies/EnemyHealthBar.gd
+++ scripts/enemies/EnemyHealthBar.gd
@@ -6,8 +6,16 @@ func _ready():
 	if body is CanvasLayer:
 		body = $"../.."
 	max_value = body.health
+	Events.switch_world.connect(_on_switch_world)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	value = body.health
+
+func _on_switch_world(normalWorld : bool):
+	if (normalWorld):
+		visible = true
+	if (not normalWorld):
+		visible = false
+	
